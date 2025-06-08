a75044709547ebbfc2b49e2b8459c8aaa99719b6
Martin Macak <martin.macak@gmail.com>
2025-06-08 22:28:18 +0200

    Add aws_resources entry point and corresponding test
    - Updated `pyproject.toml` to include a new entry point for `aws_resources` in pytest.
    - Added a test for the `mock_aws_resources` fixture to validate its functionality in `test_fixtures.py`.

---
+++ aws_sam_testing/pytest/aws_resources.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+from typing import Generator
+
+import boto3
+import pytest
+
+
+class ResourceManager:
+    def __init__(
+        self,
+        session: boto3.Session,
+        working_dir: Path | None,
+        template_name: str = "template.yaml",
+    ):
+        from aws_sam_testing.aws_resources import AWSResourceManager
+        from aws_sam_testing.cfn import load_yaml_file
+        from aws_sam_testing.util import find_project_root
+
+        if working_dir is None:
+            working_dir = Path(__file__).parent
+
+        project_root = find_project_root(working_dir)
+        template = load_yaml_file(str(project_root / template_name))
+
+        self.session = session
+        self.manager = AWSResourceManager(
+            session=session,
+            template=template,
+        )
+
+    def __enter__(self):
+        self.manager.__enter__()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.manager.__exit__(exc_type, exc_value, traceback)
+
+
+@pytest.fixture
+def mock_aws_resources(
+    request,
+    mock_aws_session: boto3.Session,
+) -> Generator[ResourceManager, None, None]:
+    working_dir = Path(request.node.fspath.dirname)
+    assert working_dir.exists()
+
+    with ResourceManager(
+        session=mock_aws_session,
+        working_dir=working_dir,
+    ) as manager:
+        yield manager
--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -4,3 +4,7 @@ def test_mock_aws_session(mock_aws_session):
 
 def test_mock_aws_lambda_context(mock_aws_lambda_context):
     assert mock_aws_lambda_context is not None
+
+
+def test_mock_aws_resources(mock_aws_resources):
+    assert mock_aws_resources is not None
