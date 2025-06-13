2bf5a237f6083f6a8671794be9562caff979be66
Tomas Mencner <mencnert@gmail.com>
2025-06-13 15:51:56 +0200

    fix region constraint

--- tests/test_moto_server.py
+++ tests/test_moto_server.py
@@ -9,10 +9,15 @@ class TestMotoServer:
             moto_server.wait_for_start()
 
             s3 = boto3.client("s3", endpoint_url=f"http://127.0.0.1:{moto_server.port}")
-            s3.create_bucket(
-                Bucket="test-bucket",
-                CreateBucketConfiguration={"LocationConstraint": aws_region},
-            )
+            if aws_region == "us-east-1":
+                s3.create_bucket(
+                    Bucket="test-bucket",
+                )
+            else:
+                s3.create_bucket(
+                    Bucket="test-bucket",
+                    CreateBucketConfiguration={"LocationConstraint": aws_region},
+                )
             s3.put_object(Bucket="test-bucket", Key="test-key", Body=b"test-body")
             response = s3.get_object(Bucket="test-bucket", Key="test-key")
             assert response["Body"].read() == b"test-body"
