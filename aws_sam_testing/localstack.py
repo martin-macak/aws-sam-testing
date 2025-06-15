2e4720c9bce9452b9bc5cc1cb372bf22a11cd166
Martin Macak <martin.macak@gmail.com>
2025-06-15 12:32:09 +0200

    Refactor LocalStack toolkit to enhance build process and layer management
    - Renamed `start_localstack` method to `build` for clarity and updated its functionality to create a build directory for LocalStack.
    - Introduced `_process_lambda_layers` method to handle AWS Lambda layers, allowing for flattening and caching of layers during the build process.
    - Improved error handling and ensured proper cleanup of temporary files after processing.
    - Updated the method to return the build directory, facilitating better integration with LocalStack features.

--- aws_sam_testing/localstack.py
+++ aws_sam_testing/localstack.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from pathlib import Path
 
 from aws_sam_testing.cfn import CloudFormationTemplateProcessor
 from aws_sam_testing.core import CloudFormationTool
@@ -47,19 +48,134 @@ class LocalStack:
 
 
 class LocalStackToolkit(CloudFormationTool):
-    def start_localstack(
+    def build(
         self,
         feature_set: LocalStackFeautureSet = LocalStackFeautureSet.NORMAL,
-    ) -> LocalStack:
-        template_processor = LocalStackCloudFormationTemplateProcessor(
+        build_dir: Path | None = None,
+    ) -> Path:
+        from aws_sam_testing.aws_sam import AWSSAMToolkit
+        from aws_sam_testing.cfn import dump_yaml
+
+        base_build_dir = build_dir
+        if build_dir is None:
+            base_build_dir = Path(self.working_dir) / ".aws-sam"
+        elif isinstance(base_build_dir, str):
+            base_build_dir = Path(build_dir)
+
+        assert base_build_dir is not None
+        localstack_build_dir = base_build_dir / ".aws-sam" / "aws-sam-testing-localstack-build"
+        localstack_build_dir.mkdir(parents=True, exist_ok=True)
+
+        processor = LocalStackCloudFormationTemplateProcessor(
             template=self.template,
         )
 
         if feature_set == LocalStackFeautureSet.NORMAL:
-            template_processor.remove_non_pro_resources()
+            processor.remove_non_pro_resources()
 
-        # TODO: Use template_processor.processed_template to start LocalStack with the modified template
-        return LocalStack()
+        template = processor.processed_template
+        sam_build_dir: Path | None = None
+
+        localstack_template_path = base_build_dir / "template.localstack.yaml"
+        try:
+            dump_yaml(template, stream=localstack_template_path)
+            dump_yaml(template, stream=localstack_build_dir / "template.source.yaml")
+
+            sam_toolkit = AWSSAMToolkit(
+                working_dir=self.working_dir,
+                template_path=self.template_path,
+            )
+
+            sam_build_dir = sam_toolkit.sam_build(
+                build_dir=localstack_build_dir,
+            )
+        finally:
+            # delete the file
+            localstack_template_path.unlink()
+
+        assert sam_build_dir is not None
+
+        if feature_set == LocalStackFeautureSet.NORMAL:
+            self._process_lambda_layers(
+                source_template_path=sam_build_dir / "template.yaml",
+                build_dir=localstack_build_dir,
+                flatten_layers=True,
+                layer_cache_dir=base_build_dir / ".aws-sam" / "aws-sam-testing-localstack-layers",
+            )
+
+        return localstack_build_dir
+
+    def _process_lambda_layers(
+        self,
+        source_template_path: Path,
+        build_dir: Path,
+        flatten_layers: bool = True,
+        layer_cache_dir: Path | None = None,
+    ) -> Path:
+        """
+        Processes the AWS SAM build directory so it can be executed safely in localstack.
+        Localstack without PRO features does not support AWS Lambda layers.
+        This can be solved by downloading all lambda layers referenced in the template and then packing all packages
+        in correct order in each function.
+        This works exactly as the layered file system. So all files and packages are incrementally merged into the final
+        directory.
+
+        For example:
+        Lambda function SomeFunction has a layers layer_a and layer_b.
+        The layer_a has a package package_a
+        The layer_b has a package package_b.
+        The function also packages package_c.
+
+        After the processing, if flatten_layers is True, the function will have a directory with the following structure:
+        SomeFunction/
+            package_a/
+            package_b/
+            package_c/
+
+        If flatten_layers is False, the function will have a directory with the following structure:
+        SomeFunction/
+            package_c/
+
+
+        Also if flatten_layers was performed, all flattened layers are removed from the template
+        For example:
+        SomeFunction:
+            Type: AWS::Serverless::Function
+            Properties:
+                Layers:
+                    - !Ref LayerA
+                    - !Ref LayerB
+                PackageType: Zip
+                CodeUri: SomeFunction/
+
+        After the processing, the template will have the following structure:
+        SomeFunction:
+            Type: AWS::Serverless::Function
+            Properties:
+                PackageType: Zip
+                CodeUri: SomeFunction/
+
+        Flattening is performed in the directory given by the source_template_path. The algorithm reads
+        all lambda functions in the template and then for each function it reads the layers and packages.
+        It then processes them into new directory given in build_dir.
+
+        The source_template_path is not modified as well as the lambda functions in the source template.
+        New build is created in the build_dir. This build is valid AWS SAM build that can be executed in localstack.
+
+        Layers are downloaded from AWS so valid AWS session is required when this function is called.
+        Layers are downloaded only once and then cached in the layer_cache_dir.
+
+        Args:
+            source_template_path (Path): The source directory with input SAM build that will be processed.
+            build_dir (Path): The destination directory where the SAM build with processed build.
+            flatten_layers (bool, optional): _description_. Defaults to True.
+            layer_cache_dir (Path, optional): The directory where the layers will be cached. Defaults to None.
+
+        Returns:
+            Path: The path to the new build directory.
+        """
+
+        return build_dir
 
 
 class LocalStackCloudFormationTemplateProcessor(CloudFormationTemplateProcessor):
